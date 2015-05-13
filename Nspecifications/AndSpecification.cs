e4910e4b06f1858ecc129b552cb1415329538e8e
jnicolau <joao@nicolauoliveira.com>
2015-05-14 00:51:17 +0100

    Renamed projecto to NSpecifications

---
+++ Nspecifications/AndSpecification.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSpecifications
+{
+    internal class AndSpecification<TEntity> : Specification<TEntity>
+    {
+        protected Specification<TEntity> Spec1 { get; private set; }
+
+        protected Specification<TEntity> Spec2 { get; private set; }
+
+        internal AndSpecification(Specification<TEntity> spec1, Specification<TEntity> spec2)
+        {
+            if (spec1 == null)
+                throw new ArgumentNullException("spec1");
+
+            if (spec2 == null)
+                throw new ArgumentNullException("spec2");
+
+            Spec1 = spec1;
+            Spec2 = spec2;
+        }
+
+        public override Expression<Func<TEntity, bool>> Expression
+        {
+            get { return Spec1.Expression.And(Spec2.Expression); }
+        }
+
+        public override string ToString()
+        {
+            return "({" + Spec1.ToString() + "} And {" + Spec2.ToString() + "})";
+        }
+    }
+}
---
+++ Nspecifications/ISpecification.cs
@@ -0,0 +1,11 @@
+﻿namespace NSpecifications
+{
+    public interface ISpecification
+    {
+    }
+
+    public interface ISpecification<in TEntity> : ISpecification
+    {
+        bool IsSatisfiedBy(TEntity entity);
+    }
+}
