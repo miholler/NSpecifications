f6af1fc8b2ebf41de4c9cfe835480e9531671984
jnicolau <joao@nicolauoliveira.com>
2015-05-17 19:13:52 +0100

    Added some tests.

---
+++ NSpecifications.Tests/Entities/Drink.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSpecifications.Tests.Entities
+{
+    public class Drink
+    {
+        public Drink()
+        {
+            With = new List<String>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime ManufacturedOn { get; set; }
+
+        public List<string> With { get; set; }
+
+        internal static Drink ColdWhiskey(int id = default(int))
+        {
+            return new Drink
+            {
+                Id = id,
+                Name = "Whiskey",
+                With = { "Ice" },
+                ManufacturedOn = DateTime.Now.AddYears(-11),
+            };
+        }
+
+        internal static Drink AppleJuice(int id = default(int))
+        {
+            return new Drink
+            {
+                Id = id,
+                Name = "Apple Juice",
+                ManufacturedOn = DateTime.Now.AddMonths(-1),
+            };
+        }
+
+        internal static Drink OrangeJuice(int id = default(int))
+        {
+            return new Drink
+            {
+                Id = id,
+                Name = "Orange Juice",
+                ManufacturedOn = DateTime.Now.AddMonths(-1),
+            };
+        }
+
+        internal static Drink BlackberryJuice(int id = default(int))
+        {
+            return new Drink
+            {
+                Id = id,
+                Name = "Backberry Juice",
+                ManufacturedOn = DateTime.Now.AddMonths(-1),
+            };
+        }
+    }
+}
---
+++ NSpecifications.Tests/Interface/CompositionTests.cs
@@ -0,0 +1,73 @@
+﻿using NSpecifications.Tests.Entities;
+using NSpecifications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace NSpecifications.Tests.Interface
+{
+    [TestFixture]
+    public class CompositionTests
+    {
+
+
+        [Test]
+        public void WhiskeyAndCold()
+        {
+            // Arrange
+            Drink coldWhiskey = Drink.ColdWhiskey();
+            Drink appleJuice = Drink.AppleJuice();
+            ISpecification<Drink> whiskeySpec = new Specification<Drink>(d => d.Name.ToLower() == "whiskey");
+            ISpecification<Drink> coldSpec = new Specification<Drink>(d => d.With.Any(w => w.ToLower() == "ice"));
+
+            // Act
+            var coldWhiskeySpec = whiskeySpec.And(coldSpec);
+
+            // Assert
+            coldWhiskeySpec.IsSatisfiedBy(coldWhiskey).Should().BeTrue();
+            coldWhiskeySpec.IsSatisfiedBy(appleJuice).Should().BeFalse();
+        }
+
+        [Test]
+        public void AppleOrOrangeJuice()
+        {
+            // Arrange
+            Drink blackberryJuice = Drink.BlackberryJuice();
+            Drink appleJuice = Drink.AppleJuice();
+            Drink orangeJuice = Drink.OrangeJuice();
+            ISpecification<Drink> juiceSpec = new Specification<Drink>(d => d.With.Any(w => w.ToLower().Contains("juice")));
+            ISpecification<Drink> appleSpec = new Specification<Drink>(d => d.With.Any(w => w.ToLower().Contains("apple")));
+            ISpecification<Drink> orangeSpec = new Specification<Drink>(d => d.With.Any(w => w.ToLower().Contains("orange")));
+
+            // Act
+            var appleOrOrangeJuiceSpec = juiceSpec.And(appleSpec.Or(orangeSpec));
+
+            // Assert
+            appleOrOrangeJuiceSpec.IsSatisfiedBy(appleJuice).Should().BeTrue();
+            appleOrOrangeJuiceSpec.IsSatisfiedBy(orangeJuice).Should().BeTrue();
+            appleOrOrangeJuiceSpec.IsSatisfiedBy(blackberryJuice).Should().BeFalse();
+        }
+
+        [Test]
+        public void And()
+        {
+            // Arrange
+            Drink coldWhiskey = Drink.ColdWhiskey();
+            Drink appleJuice = Drink.AppleJuice();
+            ISpecification<Drink> whiskeySpec = new Specification<Drink>(d => d.Name.ToLower() == "whiskey");
+            ISpecification<Drink> coldSpec = new Specification<Drink>(d => d.With.Any(a => a.ToLower() == "ice"));
+
+            // Act
+            var coldWhiskeySpec = whiskeySpec.And(coldSpec);
+
+            // Assert
+            coldWhiskeySpec.IsSatisfiedBy(coldWhiskey).Should().BeTrue();
+            coldWhiskeySpec.IsSatisfiedBy(appleJuice).Should().BeFalse();
+        }
+
+    }
+}
